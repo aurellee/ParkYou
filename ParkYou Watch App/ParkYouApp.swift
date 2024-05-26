ce798eb3e72e3302c5ef668aba65d33f4a3bc8ec
lele <aurelialangi06@gmail.com>
2024-05-27 04:13:51 +0700

    Initial Commit

---
+++ ParkYou Watch App/ParkYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkYouApp.swift
+//  ParkYou Watch App
+//
+//  Created by Jaqueline Aurelia Langi on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ParkYou_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
